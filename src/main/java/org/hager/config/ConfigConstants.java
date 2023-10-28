f21a70ffe997bc33212102bf7e4e7ab5e62df077
Hager <hagar.testing@gmail.com>
2023-10-28 18:11:06 +0300

    Create StgEnvironment.properties and handle congiguration files

---
+++ src/main/java/org/hager/config/ConfigConstants.java
@@ -0,0 +1,11 @@
+package org.hager.config;
+
+
+public class ConfigConstants {
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String EDGE = "edge";
+    public static final String BROWSER = "browser";
+    public static final String BASE_URL = "base_url";
+
+}
---
+++ src/main/java/org/hager/config/ConfigurationManager.java
@@ -0,0 +1,44 @@
+package org.hager.config;
+
+import org.hager.utils.CustomAutomationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.hager.config.ConfigConstants.BASE_URL;
+import static org.hager.config.ConfigConstants.BROWSER;
+
+public class ConfigurationManager {
+    private static final Properties properties;
+    static String projectPath = System.getProperty("user.dir");
+    private static final String CONFIG_FILE_PATH = "//src//main//resources//StgEnvironment.properties";
+    private static final Logger logger = LoggerFactory.getLogger(ConfigurationManager.class);
+
+    static {
+        properties = new Properties();
+        loadPropertiesData();
+    }
+
+    public static void loadPropertiesData(){
+        try {
+            InputStream input = new FileInputStream( projectPath + CONFIG_FILE_PATH);
+            properties.load(input);
+            input.close();
+        } catch (Exception e) {
+            logger.error("An error occurred while loading configuration properties.", e);
+            // Throw a custom exception for better error handling
+            throw new CustomAutomationException("An error occurred while loading configuration properties.", e);
+        }
+    }
+
+    public static String getBrowserName() {
+        return properties.getProperty(BROWSER);
+    }
+    public static String getBaseUrl() {
+        return properties.getProperty(BASE_URL);
+    }
+}
+
