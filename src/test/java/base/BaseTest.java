34dc76fdbd4494743e663f59197c2aa09379927d
Hager <hagar.testing@gmail.com>
2023-11-01 23:20:53 +0200

    Update tearDown scope

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 
@@ -41,8 +42,8 @@ public class BaseTest {
     }
 
 
-    @AfterTest
+    @AfterClass
     public void tearDown(){
-       // driver.close();
+        driver.close();
     }
 }
