73a43f660fbb6309d463b6087ca200404fdd1a67
Hager <hagar.testing@gmail.com>
2024-02-03 08:44:24 +0200

    Udate data reader

--- src/test/java/com/ecommerce/data/reader/DataReader.java
+++ src/test/java/com/ecommerce/data/reader/DataReader.java
@@ -7,21 +7,42 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.ecommerce.constants.DataFilesPathConstants.DATA_FILE_PATH;
 
 
 public class DataReader {
+
     public static String projectPath = System.getProperty("user.dir");
+
     private static final String DATA_FILE_PATH = "//src//test//java//com//ecommerce//data//files//";
 
     public List<HashMap<String, String>> convertJsonDataToMap(String fileName) throws IOException {
+
         String jsonContent = FileUtils.readFileToString(new File(projectPath + DATA_FILE_PATH + fileName), StandardCharsets.UTF_8);
-
         ObjectMapper mapper = new ObjectMapper();
-
         return mapper.readValue(jsonContent, new TypeReference<>() {
 
         });
+
     }
+
+
+    public Object[][] provideDataFromJson(String dataFilePath) throws IOException {
+
+        List<HashMap<String, String>> map;
+        map = convertJsonDataToMap(dataFilePath);
+        Object[][] data = new Object[map.size()][1];
+        for (int i = 0; i < map.size(); i++) {
+            data[i][0] = map.get(i);
+        }
+        return data;
+
+    }
+
 }
--- src/test/java/com/ecommerce/testcases/CheckoutTest.java
+++ src/test/java/com/ecommerce/testcases/CheckoutTest.java
@@ -9,39 +9,36 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 
-import static com.ecommerce.constants.DataFilesPathConstants.DATA_FILE_PATH;
+import static com.ecommerce.constants.DataFilesPathConstants.CHECKOUT_DATA_FILE_PATH;
 import static com.ecommerce.utils.ConfigUtils.getEmail;
 import static com.ecommerce.utils.ConfigUtils.getPassword;
 
 public class CheckoutTest extends BaseTest {
 
     @Test(dataProvider = "checkoutData" )
-    public void CheckIfCheckoutProcessWorksSuccessfully(String countryName) {
+    public void CheckIfCheckoutProcessWorksSuccessfully(HashMap<String, String> data ) {
         Boolean message = new LoginPage(getDriver())
                 .load()
                 .login(getEmail(), getPassword())
                 .addFirstProductToCart()
                 .goToCartPage()
                 .clickOnCheckoutButton()
-                .selectCountry(countryName)
+                .selectCountry(data.get("countryName"))
                 .clickPlaceOrderButton()
                 .isThankYouForPlacingOrderDisplayed();
 
         Assert.assertTrue(message);
-
-
     }
+
 
 
     @DataProvider(name = "checkoutData")
     public Object[][] checkoutData() throws IOException {
         DataReader reader = new DataReader();
-        List<HashMap<String, String>> map;
-        map = reader.convertJsonDataToMap(DATA_FILE_PATH);
-        return new Object[][] {{map.get(0)}
-        };
+        return reader.provideDataFromJson(CHECKOUT_DATA_FILE_PATH);
     }
+
+
 }
 
